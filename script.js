47f15013599be3ecae0ac8ba8117461b044284ce
aayushpandey04 <aayushpandey4250@gmail.com>
2026-04-06 03:02:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyABorOcuj8UV4GMMJ3qUL3Do5lFRYxaySE",
-    authDomain: "internship-portal-ddcb3.firebaseapp.com",
-    projectId: "internship-portal-ddcb3",
-    storageBucket: "internship-portal-ddcb3.firebasestorage.app",
-    messagingSenderId: "287388548957",
-    appId: "1:287388548957:web:2370cc6b4fa8e54096b2b4",
-    measurementId: "G-W538342FVS"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyABorOcuj8UV4GMMJ3qUL3Do5lFRYxaySE",
+  authDomain: "internship-portal-ddcb3.firebaseapp.com",
+  projectId: "internship-portal-ddcb3",
+  storageBucket: "internship-portal-ddcb3.firebasestorage.app",
+  messagingSenderId: "287388548957",
+  appId: "1:287388548957:web:2370cc6b4fa8e54096b2b4"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.goToApply = function () {
+    window.location.href = "apply.html";
+}
+
+window.goHome = function () {
+    window.location.href = "index.html";
+}
+
+window.submitForm = async function () {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let skills = document.getElementById("skills").value;
+
+    if (!name || !email) {
+        document.getElementById("error").innerText = "Please fill all required fields";
+        return;
+    }
+
+    try {
+        await addDoc(collection(db, "applications"), {
+            name: name,
+            email: email,
+            phone: phone,
+            skills: skills,
+            time: new Date()
+        });
+
+        window.location.href = "success.html";
+
+    } catch (error) {
+        alert("Error submitting form");
+        console.error(error);
+    }
+};
