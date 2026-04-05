4c09ca10ff183f837f7b1bc7cb77df61ba554b96
aayushpandey04 <aayushpandey4250@gmail.com>
2026-04-06 03:33:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyABorOcuj8UV4GMMJ3qUL3Do5lFRYxaySE",
@@ -13,15 +13,28 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+window.goToApply = function () {
+    window.location.href = "apply.html";
+};
+
+window.goHome = function () {
+    window.location.href = "index.html";
+};
+
 window.submitForm = async function () {
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let phone = document.getElementById("phone").value;
+    let gender = document.getElementById("gender").value;
+    let college = document.getElementById("college").value;
+    let city = document.getElementById("city").value;
+    let level = document.getElementById("level").value;
     let skills = document.getElementById("skills").value;
+    let why = document.getElementById("why").value;
 
     if (!name || !email) {
-        document.getElementById("error").innerText = "Please fill all required fields";
+        document.getElementById("error").innerText = "Fill required fields";
         return;
     }
 
@@ -30,14 +43,19 @@ window.submitForm = async function () {
             name,
             email,
             phone,
+            gender,
+            college,
+            city,
+            level,
             skills,
+            why,
             time: new Date()
         });
 
         window.location.href = "success.html";
 
-    } catch (error) {
+    } catch (e) {
         alert("Error submitting form");
-        console.error(error);
+        console.log(e);
     }
 };
