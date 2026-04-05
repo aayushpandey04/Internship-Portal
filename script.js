b281008bea9e50ef692b0d608e1e06bacd94849d
aayushpandey04 <aayushpandey4250@gmail.com>
2026-04-06 02:49:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-function goToApply() {
-    window.location.href = "apply.html";
-}
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-function goHome() {
-    window.location.href = "index.html";
-}
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyABorOcuj8UV4GMMJ3qUL3Do5lFRYxaySE",
+    authDomain: "internship-portal-ddcb3.firebaseapp.com",
+    projectId: "internship-portal-ddcb3",
+    storageBucket: "internship-portal-ddcb3.firebasestorage.app",
+    messagingSenderId: "287388548957",
+    appId: "1:287388548957:web:2370cc6b4fa8e54096b2b4",
+    measurementId: "G-W538342FVS"
+  };
 
-function submitForm() {
-    let name = document.getElementById("name").value;
-
-    if (!name) {
-        document.getElementById("error").innerText = "Fill required fields";
-        return;
-    }
-
-    window.location.href = "success.html";
-}
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
